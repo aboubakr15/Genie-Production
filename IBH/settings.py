5ef25d8ef6b37d2f9b1381aa1afe33e05d92ce36
Aboubakr <abobkrahmed99@gmail.com>
2025-09-24 03:26:50 +0300

    db

--- IBH/settings.py
+++ IBH/settings.py
@@ -120,9 +120,9 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'railway',
         'USER': 'root',
-        'PASSWORD': 'QcieGmwVSuAnLuXBRLKpImOJhKIWSEjR',  # MySQL server pass is 'admin@ibh'
-        'HOST': 'yamabiko.proxy.rlwy.net',  # or the IP address of your MySQL server '192.168.0.200'
-        'PORT': '17784',
+        'PASSWORD': 'TApxnYPsQxmlTrQYTRnkaXEOCIbGewaK',  # MySQL server pass is 'admin@ibh'
+        'HOST': 'mysql.railway.internal',  # or the IP address of your MySQL server '192.168.0.200'
+        'PORT': '29547',
         'OPTIONS': {
             'charset': 'utf8mb4',
             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
